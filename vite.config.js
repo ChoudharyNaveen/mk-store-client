3855daa6001ab61db71e05f409e123c7602b36a1
chowdrysai <naveen.choudhary@flowone.in>
2026-01-06 23:11:29 +0530

    fix: deployment #11

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Build inside current project folder
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  base: '/', // use '/' if deployed at root domain, or '/subfolder/' if in a subdirectory
+  build: {
+    outDir: 'build_temp', // relative to project root
+    emptyOutDir: true,
+    rollupOptions: {
+      input: 'index.html', // relative path
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
+  },
 });
